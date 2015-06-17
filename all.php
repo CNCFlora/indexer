900edf4cfae3199f2817c856a3a9aafad8ab5df9
Laura Moraes <lmoraes@cncflora.net>
2015-06-17 10:50:12 -0300

    Delete and create index in ElasticSearch database to recreate mapping from scratch

--- all.php
+++ all.php
@@ -1,6 +1,7 @@
 <?php
 
-$all = json_decode(file_get_contents("http://cncflora.jbrj.gov.br/couchdb/_all_dbs"));
+//$all = json_decode(file_get_contents("http://cncflora.jbrj.gov.br/couchdb/_all_dbs"));
+$all = json_decode(file_get_contents("http://jb048.jbrj.gov.br/couchdb/_all_dbs"));
 
 foreach($all as $db) {
   if(!preg_match("/^_/",$db) && !preg_match("/_history$/",$db)) {
--- reindex.php
+++ reindex.php
@@ -7,8 +7,10 @@ if(!isset($argv[1])) {
 
 $db = $argv[1];
 
-define("COUCHDB","http://cncflora.jbrj.gov.br/couchdb/".$db);
-define("ELASTICSEARCH","http://cncflora.jbrj.gov.br/elasticsearch/".$db);
+//define("COUCHDB","http://cncflora.jbrj.gov.br/couchdb/".$db);
+//define("ELASTICSEARCH","http://cncflora.jbrj.gov.br/elasticsearch/".$db);
+define("COUCHDB","http://jb048.jbrj.gov.br/couchdb/".$db);
+define("ELASTICSEARCH","http://jb048.jbrj.gov.br/elasticsearch/".$db);
 
 echo "Database: ".$db."\n";
 
@@ -23,6 +25,30 @@ if(isset($argv[2])) {
   $all = json_decode(file_get_contents($db.".json"));
 }
 
+$curl = curl_init(ELASTICSEARCH);
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: 0'));
+
+// Make the REST call, returning the result
+$response = curl_exec($curl);
+if (!$response) {
+    die("Connection Failure.n");
+}
+echo $response;
+
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: 0'));
+
+// Make the REST call, returning the result
+$response = curl_exec($curl);
+if (!$response) {
+    die("Connection Failure.n");
+}
+echo $response;
 
 $docs = [];
 foreach($all->rows as $row) {
@@ -35,5 +61,3 @@ foreach($all->rows as $row) {
     $opts = ['http'=>['method'=>'POST','content'=>$json,'header'=>'Content-type: application/json']];
     echo file_get_contents(ELASTICSEARCH."/".$doc->metadata->type."/".urlencode($doc->id), NULL, stream_context_create($opts))."\n";
 }
-
-
