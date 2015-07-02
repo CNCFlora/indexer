3b0805272f120a82749965af2caf4f433c0bac15
Laura Moraes <lmoraes@cncflora.net>
2015-07-02 15:25:36 -0300

    Adapt script to index all databases

--- all.php
+++ all.php
@@ -1,10 +1,15 @@
 <?php
 
-//$all = json_decode(file_get_contents("http://cncflora.jbrj.gov.br/couchdb/_all_dbs"));
-$all = json_decode(file_get_contents("http://jb048.jbrj.gov.br/couchdb/_all_dbs"));
+if(!isset($argv[1])) {
+    echo "Which host?\n";
+  exit;
+}
+$host = $argv[1];
+define("COUCHDB", $host.':5984/');
+$all = json_decode(file_get_contents(COUCHDB."_all_dbs"));
 
 foreach($all as $db) {
   if(!preg_match("/^_/",$db) && !preg_match("/_history$/",$db)) {
-    passthru("php reindex.php ".$db);
+    passthru("php reindex.php ".$host." ".$db);
   }
 }
